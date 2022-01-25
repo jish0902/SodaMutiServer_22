04941f134c9568e592950de14461be5a395151e8
jish0902 <84966162+jish0902@users.noreply.github.com>
2022-01-26 01:06:28 +0900

    Queue Listener Connector

---
+++ ServerCore/Connector.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Connector
+    {
+        
+        Func<Session> _sessionFactory;
+
+        public void Connect(IPEndPoint iPEndPoint, Func<Session> sessionFactory,int number = 1)
+        {
+
+            for (int i = 0; i < number; i++)
+            {
+                Socket Socket = new Socket(iPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _sessionFactory = sessionFactory;
+
+                
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += OnConnectCompleted;
+                args.RemoteEndPoint = iPEndPoint;
+                args.UserToken = Socket;
+
+                RegisterConnect(args);
+
+                //temp
+                Thread.Sleep(10);
+            }
+
+            //args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+
+        }
+
+        public void RegisterConnect(SocketAsyncEventArgs args)
+        {
+            Socket socket = (Socket)args.UserToken;
+            if (socket == null)
+                return;
+
+            try
+            {
+                bool pending = socket.ConnectAsync(args);
+                if (pending == false)
+                    OnConnectCompleted(null, args);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+
+        }
+
+
+        public void OnConnectCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            try
+            {
+                if (args.SocketError == SocketError.Success)
+                {
+                    Session session = _sessionFactory.Invoke();
+                    session.Start(args.ConnectSocket);
+                    session.OnConnected(args.ConnectSocket.RemoteEndPoint);
+                }
+                else
+                    Console.WriteLine(args.SocketError.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ ServerCore/Listener.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    public class Listener
+    {
+        Socket _listenSocket;
+        Func<Session> _sessionFactory;
+
+        public void Init(IPEndPoint iPEndPoint,Func<Session> sessionFactory, int register = 10, int backlog = 10)
+        {
+            _listenSocket = new Socket(iPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _sessionFactory = sessionFactory;
+
+            // 문지기 교육
+            _listenSocket.Bind(iPEndPoint);
+
+            //영업 시작
+            _listenSocket.Listen(backlog);
+
+            for (int i = 0; i < register; i++)
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += OnAcceptCompleted;
+                RegisterAccept(args);
+            }
+            
+            //args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+
+        }
+
+        public void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+            try
+            {
+                bool pending = _listenSocket.AcceptAsync(args);
+                if (pending == false)
+                    OnAcceptCompleted(null, args);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+            
+
+        }
+
+
+        public void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            try
+            {
+                if (args.SocketError == SocketError.Success)
+                {
+                    Session session = _sessionFactory.Invoke();
+                    session.Start(args.AcceptSocket);
+                    session.OnConnected(args.AcceptSocket.RemoteEndPoint);
+                }
+                else
+                    Console.WriteLine(args.SocketError.ToString());
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            RegisterAccept(args);
+        }
+
+    }
+}
